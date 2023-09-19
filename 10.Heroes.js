3c70df504e814da6bb7701b6964dc3b4223420f3
jahraBg <145470623+jahraBg@users.noreply.github.com>
2023-09-19 17:42:11 +0300

    Add files via upload

---
+++ 10.Heroes.js
@@ -0,0 +1,41 @@
+function solve (){
+    let health = 100
+    let comon = 100 
+let create = {
+  fighter: (name)=>{
+    return ( obj = {
+        name,
+        health,
+        stamina: comon,
+        fight(){
+            this.stamina--
+            console.log(`${name} slashes at the foe!`)
+        }
+    })
+  },
+ mage : (name) =>{
+    return (obj ={
+        name,
+        health,
+        mana : comon,
+        cast (spell){
+            this.mana--
+            console.log(`${name} cast ${spell}`)
+        }
+    })
+ }
+}
+
+return create
+
+
+}
+let create = solve();
+const scorcher = create.mage("Scorcher"); 
+scorcher.cast("fireball")
+ scorcher.cast("thunder")
+  scorcher.cast("light")
+const scorcher2 = create.fighter("Scorcher 2");
+ scorcher2.fight()
+ console.log(scorcher2.stamina);
+console.log(scorcher.mana);
